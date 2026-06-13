e37a973ae34db2e834b0c66ae637d920be2d0cee
Ay-sha-07 <ayishamuhammed2005@gmail.com>
2026-06-13 20:38:54 +0530

    chore: fix prettier formatting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,15 +12,14 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends('next/core-web-vitals'),
   {
-    
     ignores: [
-      ".next/**/*",
-      "node_modules/**/*",
-      "out/**/*",
-      "build/**/*",
-      "linkFBtoMDB/**/*" 
+      '.next/**/*',
+      'node_modules/**/*',
+      'out/**/*',
+      'build/**/*',
+      'linkFBtoMDB/**/*',
     ],
   },
 ];
