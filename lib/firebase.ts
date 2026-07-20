6ebd63d80ff52520361bacf039d656f15a7aa70f
TanCodeX <tanmaypatwary@gmail.com>
2026-07-20 11:39:51 +0530

    fix: add fallback value for firebase api key in configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'fake-api-key',
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, // NOTE: .com NOT .app
